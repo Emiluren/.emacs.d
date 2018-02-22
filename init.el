bbf6d55c510864f714de4dfd29eb90bd319ae823
Emil Segerbäck <emilsegerback@gmail.com>
2018-02-22 14:46:25 +0100

    Add some stuff

--- init.el
+++ init.el
@@ -4,6 +4,8 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
+(load-theme 'zenburn t)
+
 ;; Make undo easier to use
 (global-undo-tree-mode)
 
@@ -16,6 +18,17 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+
+;; Enable column number in modeline
+ (setq column-number-mode t)
+
+;; Don't use tabs and use 4 spaces for indentation
+(setq-default indent-tabs-mode nil
+              c-basic-offset 4)
+
+;; Make Emacs split window horizontally by default
+(setq split-height-threshold nil
+	  split-width-threshold 120)
 
 ;; Use nyan-mode to replace scroll bar
 (nyan-mode 1)
@@ -36,6 +49,14 @@
 ;; Some misc key bindings
 (global-set-key (kbd "<escape>") 'keyboard-quit)
 (global-set-key (kbd "C-x g") 'magit-status)
+(setq magit-delete-by-moving-to-trash nil)
+
+;; Use .m as matlab instead of objective-c
+(add-to-list 'auto-mode-alist '("\\.m\\'" . octave-mode))
+
+;; Hide backup files and autosave files in dired
+(require 'dired-x)
+(setq-default dired-omit-files-p t)
 
 (load (expand-file-name "~/quicklisp/slime-helper.el"))
 (setq inferior-lisp-program "sbcl")
@@ -47,17 +68,31 @@
 (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
 (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
 (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+(add-hook 'slime-repl-mode-hook       #'enable-paredit-mode)
+(add-hook 'clojurescript-mode-hook    #'enable-paredit-mode)
+(add-hook 'cider-repl-mode-hook       #'enable-paredit-mode)
+
+(require 'clj-refactor)
+
+(defun my-clojure-mode-hook ()
+  (enable-paredit-mode)
+  (clj-refactor-mode 1))
+
+(add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("c3e6b52caa77cb09c049d3c973798bc64b5c43cc437d449eacf35b3e776bf85c" "5a0eee1070a4fc64268f008a4c7abfda32d912118e080e18c3c865ef864d1bea" "2a739405edf418b8581dcd176aaf695d319f99e3488224a3c495cb0f9fd814e3" default)))
  '(ediff-window-setup-function (quote ediff-setup-windows-plain))
  '(ido-enable-flex-matching t)
  '(package-selected-packages
    (quote
-	(undo-tree haskell-mode csharp-mode paredit use-package parinfer magit nyan-mode cider slime))))
+    (alchemist elixir-mode apropospriate-theme glsl-mode clj-refactor geiser zenburn-theme undo-tree haskell-mode csharp-mode paredit use-package parinfer magit nyan-mode cider slime))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
