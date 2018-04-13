23683ffaee31b8eae91f63bd1e652aac538a817c
Emil Segerbäck <emilsegerback@gmail.com>
2018-04-13 09:25:09 +0200

    Move custom to a separate file

--- init.el
+++ init.el
@@ -125,30 +125,8 @@
 
 (add-hook 'org-mode-hook 'turn-on-auto-fill)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("77c3f5f5acaa5a276ca709ff82cce9b303f49d383415f740ba8bcc76570718b9" "bbb4a4d39ed6551f887b7a3b4b84d41a3377535ccccf901a3c08c7317fad7008" "90bd0eb20a1cb155b5a076f698b3c72cfe775aa7ea93b7bfbc171eb250db5e20" "5e52ce58f51827619d27131be3e3936593c9c7f9f9f9d6b33227be6331bf9881" "086970da368bb95e42fd4ddac3149e84ce5f165e90dfc6ce6baceae30cf581ef" "0e0c37ee89f0213ce31205e9ae8bce1f93c9bcd81b1bcda0233061bb02c357a8" "c3e6b52caa77cb09c049d3c973798bc64b5c43cc437d449eacf35b3e776bf85c" "5a0eee1070a4fc64268f008a4c7abfda32d912118e080e18c3c865ef864d1bea" "2a739405edf418b8581dcd176aaf695d319f99e3488224a3c495cb0f9fd814e3" default)))
- '(ediff-window-setup-function (quote ediff-setup-windows-plain))
- '(ido-enable-flex-matching t)
- '(lsp-ui-sideline-show-hover nil)
- '(nrepl-message-colors
-   (quote
-    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
- '(package-selected-packages
-   (quote
-    (toml-mode lsp-mode lsp-rust lsp-ui yaml-mode flycheck flycheck-clojure flycheck-crystal flycheck-elixir flycheck-elm rust-mode markdown-mode crystal-mode solaire-mode doom-themes tao-theme alchemist elixir-mode apropospriate-theme glsl-mode clj-refactor geiser zenburn-theme undo-tree haskell-mode csharp-mode paredit use-package parinfer magit nyan-mode cider slime)))
- '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
 
 (provide 'init)
 ;;; init.el ends here
