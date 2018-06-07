be3a1eb13df3492a6f916b422f095694e6a481b7
Emil Segerbäck <emilsegerback@gmail.com>
2018-06-07 18:55:09 +0200

    Set up use-package and auto-compile

--- init.el
+++ init.el
@@ -1,5 +1,8 @@
 ;;; -*- lexical-binding: t -*-
 
+;; Added by Package.el. Do not remove
+(package-initialize)
+
 (setq
 ;; Give emacs 100mb memory to use before trying to collect garbage
  gc-cons-threshold 100000000
@@ -7,15 +10,10 @@
  x-wait-for-event-timeout nil
  custom-file "~/.emacs.d/lisp/custom.el"
  )
-
-;; Make sure custom settings are set up before other stuff
-;; so that selected packages are available
 (load custom-file)
 
-;; Added by Package.el. Do not remove
-(package-initialize)
-
-(add-to-list 'load-path "~/.emacs.d/lisp")
+(add-to-list 'load-path "~/.emacs.d/lisp") ; All of my other elisp files
+(add-to-list 'load-path "~/Programmering/emacs/telega.el/") ; Telegram client
 
 ;; Load email address and stuff
 (let ((private-file "~/.emacs.d/lisp/private.el"))
@@ -23,15 +21,22 @@
    (load private-file)))
 
 ;; Enable melpa repository
-(add-to-list
- 'package-archives
- '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 
 ;; If this is a new install we need to make sure that all packages are available
 (unless (file-directory-p "~/.emacs.d/elpa")
-  (package-refresh-contents)
-  (package-install-selected-packages))
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(setq use-package-always-ensure t)
+(require 'use-package)
+
+;; Set up auto compilation
+(use-package auto-compile
+  :config (auto-compile-on-load-mode))
+(setq load-prefer-newer t)
 
 (load "~/.emacs.d/lisp/set-variables.el")
 (load "~/.emacs.d/lisp/mode-setup.el")
