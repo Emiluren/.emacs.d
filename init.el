64e1dcf2abdfc79e673f21cdb635009682905e41
Emil Segerbäck <emil.segerback@attentec.se>
2019-12-06 09:02:20 +0100

    Switch to eink-theme

--- init.el
+++ init.el
@@ -895,58 +895,67 @@ codepoints starting from codepoint-start."
 
 ;;; Theme
 ;; Set up color theme and other visual stuff.
-(use-package doom-themes
-  :demand t
+;; (use-package doom-themes
+;;   :demand t
+;;   :config
+;;   (setq doom-themes-enable-bold t ; if nil, bold is universally disabled
+;;      doom-themes-enable-italic t) ; if nil, italics is universally disabled
+
+;;   (load-theme 'doom-one t)
+
+;;   ;; Enable flashing mode-line on errors
+;;   (doom-themes-visual-bell-config)
+
+;;   ;; Corrects (and improves) org-mode's native fontification.
+;;   (doom-themes-org-config)
+
+;;   ;; Prevent lsp-face-highlight from being too distracting
+;;   (with-eval-after-load "lsp-methods"
+;;     (let ((brighter-bg (doom-lighten (face-background 'default) 0.05)))
+;;       (doom-themes-set-faces 'doom-one
+;;      (lsp-face-highlight-read :background brighter-bg)
+;;      (lsp-face-highlight-textual :background brighter-bg)
+;;      (lsp-face-highlight-write :background brighter-bg))))
+
+;;   (with-eval-after-load "rtags"
+;;     (dolist (props '((rtags-errline "red")
+;;                   (rtags-fixitline "yellow")))
+;;       (cl-destructuring-bind (face color) props
+;;      (unset-face-attributes face '(:foreground :background))
+;;      (set-face-attribute face nil :underline
+;;                          `(:color ,color :style wave)))))
+
+;;   (with-eval-after-load "em-prompt"
+;;     ;; Make the eshell prompt slightly green so it stands out
+;;     (set-face-foreground 'eshell-prompt "#9ccca4")))
+
+;; (use-package solaire-mode
+;;   :after doom-themes
+;;   :demand t
+;;   :hook ((after-change-major-mode . turn-on-solaire-mode)
+;;       (ediff-prepare-buffer . solaire-mode)
+;;       ;; ...if you use auto-revert-mode, this prevents solaire-mode from turning
+;;       ;; itself off every time Emacs reverts the file
+;;       (after-revert . turn-on-solaire-mode)
+;;       ;; highlight the minibuffer when it is activated:
+;;       (minibuffer-setup . solaire-mode-in-minibuffer))
+
+;;   :config
+;;   ;; if the bright and dark background colors are the wrong way around, use this
+;;   ;; to switch the backgrounds of the `default` and `solaire-default-face` faces.
+;;   ;; This should be used *after* you load the active theme!
+;;   ;;
+;;   ;; NOTE: This is necessary for themes in the doom-themes package!
+;;   (solaire-mode-swap-bg))
+
+(use-package eink-theme
   :config
-  (setq doom-themes-enable-bold t ; if nil, bold is universally disabled
-     doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'eink t)
+  (set-face-attribute 'font-lock-comment-face nil :weight 'light))
 
-  (load-theme 'doom-one t)
-
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config)
-
-  ;; Prevent lsp-face-highlight from being too distracting
-  (with-eval-after-load "lsp-methods"
-    (let ((brighter-bg (doom-lighten (face-background 'default) 0.05)))
-      (doom-themes-set-faces 'doom-one
-     (lsp-face-highlight-read :background brighter-bg)
-     (lsp-face-highlight-textual :background brighter-bg)
-     (lsp-face-highlight-write :background brighter-bg))))
-
-  (with-eval-after-load "rtags"
-    (dolist (props '((rtags-errline "red")
-                  (rtags-fixitline "yellow")))
-      (cl-destructuring-bind (face color) props
-     (unset-face-attributes face '(:foreground :background))
-     (set-face-attribute face nil :underline
-                         `(:color ,color :style wave)))))
-
-  (with-eval-after-load "em-prompt"
-    ;; Make the eshell prompt slightly green so it stands out
-    (set-face-foreground 'eshell-prompt "#9ccca4")))
-
-(use-package solaire-mode
-  :after doom-themes
-  :demand t
-  :hook ((after-change-major-mode . turn-on-solaire-mode)
-      (ediff-prepare-buffer . solaire-mode)
-      ;; ...if you use auto-revert-mode, this prevents solaire-mode from turning
-      ;; itself off every time Emacs reverts the file
-      (after-revert . turn-on-solaire-mode)
-      ;; highlight the minibuffer when it is activated:
-      (minibuffer-setup . solaire-mode-in-minibuffer))
-
+(use-package paren-face
   :config
-  ;; if the bright and dark background colors are the wrong way around, use this
-  ;; to switch the backgrounds of the `default` and `solaire-default-face` faces.
-  ;; This should be used *after* you load the active theme!
-  ;;
-  ;; NOTE: This is necessary for themes in the doom-themes package!
-  (solaire-mode-swap-bg))
+  (global-paren-face-mode))
 
 ;; (use-package leuven-theme
 ;;   :config
